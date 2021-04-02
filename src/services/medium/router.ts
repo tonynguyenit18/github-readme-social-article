347fc98022b3ad76c0b729dc1f1ca707b0f9c0a4
Tony Nguyen <tonynguyenit18@gmail.com>
2021-04-02 14:39:44 +0700

    feature: add get recent articles

--- src/services/medium/router.ts
+++ src/services/medium/router.ts
@@ -3,6 +3,20 @@ import { generateMediumTemplate } from "../../utils/svgTemplates"
 import { mediumRecentArticles } from "./apis"
 const router = express.Router()
 
+router.get("/:userName", async (req: Request, res: Response) => {
+  const { userName, index } = req.params
+  const articleIndex = parseInt(index)
+
+  const recentArticles = await mediumRecentArticles({ userName, index: articleIndex })
+  let response = "<div>"
+  recentArticles.forEach((mediumArticle) => {
+    const template = generateMediumTemplate({ mediumArticle })
+    response += template
+  })
+  response += "</div>"
+  res.send(response)
+})
+
 router.get("/:userName/:index", async (req: Request, res: Response) => {
   const { userName, index } = req.params
   const articleIndex = parseInt(index)
